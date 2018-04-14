7b2d8e1b5f04b5e8c9e7ce2c32b514757ea15eef
Jason Borum <33642393+jborum@users.noreply.github.com>
2018-04-14 16:43:37 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Programming_Project_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 200);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(252, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 200);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(475, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 200);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(24, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 200);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(252, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 200);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(480, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(200, 200);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "X";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(24, 477);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(200, 200);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "X";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Maroon;
+            this.label8.Location = new System.Drawing.Point(252, 477);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(200, 200);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "X";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label9.Font = new System.Drawing.Font("Vineta BT", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Maroon;
+            this.label9.Location = new System.Drawing.Point(480, 477);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(200, 200);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "X";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResults.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtResults.Location = new System.Drawing.Point(24, 701);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(651, 76);
+            this.txtResults.TabIndex = 9;
+            this.txtResults.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.Color.Maroon;
+            this.btnNewGame.Location = new System.Drawing.Point(24, 802);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(656, 58);
+            this.btnNewGame.TabIndex = 11;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClose.Location = new System.Drawing.Point(24, 866);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(656, 58);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 940);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * This program was written by Jason Borum
+ * Date: 4/14/2018
+ * Course: CST117
+ * Instructor: Dr. Brandon Bass
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            // Initilize random number object
+            Random rand = new Random();
+            string whoWon;
+            
+            //Set size of gameboard
+            const int ROWS = 3;
+            const int COLS = 3;
+
+            // Create a two-dimensional int array.
+            int[,] gameBoard = new int[ROWS, COLS];
+
+            // Fill array with random integers between 0 and 1
+            // This does not follow the actual rules of tictactoe
+            for (int row = 0; row < ROWS; row++)
+                {
+                for (int col = 0; col < COLS; col++)
+                    {
+                    gameBoard[row, col] = rand.Next(2);
+                    }
+                }
+
+            // Fill label with values from array
+            label1.Text = getValue(gameBoard[0, 0]);
+            label2.Text = getValue(gameBoard[0, 1]);
+            label3.Text = getValue(gameBoard[0, 2]);
+
+            label4.Text = getValue(gameBoard[1, 0]);
+            label5.Text = getValue(gameBoard[1, 1]);
+            label6.Text = getValue(gameBoard[1, 2]);
+
+            label7.Text = getValue(gameBoard[2, 0]);
+            label8.Text = getValue(gameBoard[2, 1]);
+            label9.Text = getValue(gameBoard[2, 2]);
+
+            txtResults.Text = getWinner(gameBoard);
+
+        } //End of btnNewGame
+      
+        // Method to determine the winner
+        public string getWinner (int[,] intArray)
+        {
+            // set default value to -1 which is equal to a tie
+            int winner = -1;
+
+            // Testing for a horizontal win
+            if ((intArray[0,0] == intArray[0,1]) && (intArray[0, 0] == intArray[0, 2]))
+            {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[1, 0] == intArray[1, 1]) && (intArray[1, 0] == intArray[1, 2]))
+            {
+                winner = intArray[1, 0];
+            }
+            if ((intArray[2, 0] == intArray[2, 1]) && (intArray[2, 0] == intArray[2, 2]))
+            {
+                winner = intArray[2, 0];
+            }
+
+
+            // Testing for a Vertical win
+            if ((intArray[0, 0] == intArray[1, 0]) && (intArray[0, 0] == intArray[2, 0]))
+            {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[0, 1] == intArray[1, 1]) && (intArray[0, 1] == intArray[2, 1]))
+            {
+                winner = intArray[0, 1];
+            }
+            if ((intArray[0, 2] == intArray[1, 2]) && (intArray[0, 2] == intArray[2, 2]))
+            {
+                winner = intArray[0, 2];
+            }
+
+
+            //Testing for a Diaganol win 
+            if ((intArray[0, 0] == intArray[1, 1]) && (intArray[0, 0] == intArray[2, 2]))
+            {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[0, 2] == intArray[1, 1]) && (intArray[0, 2] == intArray[2, 0]))
+            {
+                winner = intArray[0, 2];
+            }
+
+            // convert integet to text for winner
+            if (winner == 0)
+            {
+                return "O is the winner";
+            }
+            else if (winner == 1)
+            {
+            return "X is the winner";
+            }
+            else
+            {
+                return "The game was a tie";
+            }
+        } // End of get winner
+
+        // Get integer and replace it with string value for user
+        public string getValue (int integerValue)
+        {
+            if (integerValue == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+    } // End of form
+} // ENd of Namespace
